0c03bb8e60ebc34a322c0bf5965f5159b54d5b75
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-08-23 11:23:38 +0300

    Add logout and pagination

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -4,7 +4,7 @@ const { HttpError } = require("../helpers");
 const { User } = require("../models/user");
 const { SECRET_KEY } = process.env;
 
-const authenticate = (req, res, next) => {
+const authenticate = async (req, res, next) => {
   const { authorization = "" } = req.headers;
   const [bearer, token] = authorization.split(" ");
   if (bearer !== "Bearer") {
@@ -13,7 +13,7 @@ const authenticate = (req, res, next) => {
   try {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
-    if (!user) {
+    if (!user || !user.token || user.token !== token) {
       next(HttpError(401));
     }
     req.user = user;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { schemas } = require("../../models/user");
 const ctrl = require("../../controllers/auth");
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
+
+router.post("/logout", authenticate, ctrl.logout);
 
 module.exports = router;
