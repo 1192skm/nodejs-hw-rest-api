0c41ccdcc4333ee8e883dd7c5d2773c8c69ed931
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-08-31 16:23:13 +0300

    Add verification by ID

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,6 +6,10 @@ const { validateBody, authenticate, upload } = require("../../middlewares");
 
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
 
+router.get("/verify/:verificationCode", ctrl.verifyEmail);
+
+router.post("/verify", validateBody(schemas.emailSchema), ctrl.resendVerifyEmail)
+
 router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
